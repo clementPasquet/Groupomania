ede176f08aeed70324aa79118cc3073cac1769e3
ClementPasquet <94584059+clementPasquet@users.noreply.github.com>
2022-11-19 16:58:51 +0100

    mise a jour avant rendu

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -13,24 +13,29 @@ const CreatePost = () => {
   const [file, setFile] = useState();
   const userData = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
+  const error = useSelector((state) => state.errorReducer.postError);
 
   const submitPost = async (e) => {
+    //on crée un objet data de type FormData dans lequel on ajoute de maniere conditionnelle le file et/ou le text du post
+    //on utilise ensuite un dispatch en faisant appelle a createPost pour envoyer a la BD et au store la data du nouveu post
+    //enfin on reinitialise le state du texte et du file puis on récupère de nouveaux les informations des posts
     const data = new FormData();
     data.append("postID", userData._id);
-    if (postText) {
-      data.append("postText", postText);
+    if (postText || file) {
+      if (postText) {
+        data.append("postText", postText);
+      }
+
+      if (file) {
+        data.append("file", file);
+      }
+      await dispatch(createPost(data));
+      setPostText("");
+      setFile(undefined);
+      dispatch(getPosts());
+    } else {
+      alert("Veuillez écrire quelque chose ou importer une image ");
     }
-
-    if (file) {
-      data.append("file", file);
-
-      console.log(file);
-    }
-
-    await dispatch(createPost(data));
-    setPostText("");
-    setFile(undefined);
-    dispatch(getPosts());
   };
 
   return (
@@ -38,7 +43,7 @@ const CreatePost = () => {
       {uid ? (
         <div className="createPost__content">
           <img
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
             className="createPost__profilImage"
             src={userData.image}
             alt="user-profil"
@@ -53,9 +58,9 @@ const CreatePost = () => {
             onChange={(e) => setPostText(e.target.value)}
             value={postText}
           />
-
+          {error && <div className="createPost__postError"> {error} </div>}
           <div className="createPost__addFile">
-            <label for="file-uploadPost">
+            <label htmlFor="file-uploadPost">
               <FontAwesomeIcon className="createPost__iconImg" icon={faImage} />
 
               <input
